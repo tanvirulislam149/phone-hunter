b6623bbc7dc3c49b37258817472ed13a0208e858
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-03-02 02:54:56 +0600

    search result finished

--- app.js
+++ app.js
@@ -19,27 +19,39 @@ const displayData = (data) => {
     resultContainer.style.display = "block";
     const searchResult = document.getElementById("search-result");
     searchResult.textContent = "";
-    data.slice(0, 20).forEach(element => {
-        // console.log(element);
-        const card = document.createElement("div");
-        card.classList.add("col");
-        card.classList.add("shadow");
-        card.classList.add("bg-body");
-        card.classList.add("rounded");
-        card.innerHTML = `
-        <div class="card h-100">
-            <img src="${element.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h3 class="card-title text-success">${element.brand}</h3>
-                <h5 class="card-title text-success">${element.phone_name}</h5>
-            </div>
-            <div class="card-footer">
-                <button id="details-btn" type="button"
-                    class="btn btn-outline-success bg-success text-white fw-bold w-100">Details</button>
-            </div>
-        </div>
+    const noResultContainer = document.getElementById("no-result-container");
+    noResultContainer.textContent = "";
+    if (data.length === 0) {
+        const noResult = document.createElement("div");
+        noResult.classList.add("fs-3");
+        noResult.classList.add("fw-bold");
+        noResult.classList.add("text-center");
+        noResult.classList.add("my-5");
+        noResult.innerHTML = `
+            <h1>Sorry!!!There is no phone with this name </h1>
         `
-        searchResult.appendChild(card);
-    });
+        noResultContainer.appendChild(noResult);
+    }
+    else {
+        data.slice(0, 20).forEach(element => {
+            // console.log(element);
+            const card = document.createElement("div");
+            card.classList.add("col");
+            card.innerHTML = `
+            <div class="card h-100">
+                <img src="${element.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h3 class="card-title text-success">${element.brand}</h3>
+                    <h5 class="card-title text-success">${element.phone_name}</h5>
+                </div>
+                <div class="card-footer">
+                    <button id="details-btn" type="button"
+                        class="btn btn-outline-success bg-success text-white fw-bold w-100">Details</button>
+                </div>
+            </div>
+            `
+            searchResult.appendChild(card);
+        });
 
+    }
 }
